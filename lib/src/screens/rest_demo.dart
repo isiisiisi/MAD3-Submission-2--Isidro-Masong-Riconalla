a3f1d85c2370ac75dd21454ba9d05da832300403
cor <corchxmasong@gmail.com>
2024-06-23 17:29:16 +0800

    screen for the rest_demo

--- lib/src/screens/rest_demo.dart
+++ lib/src/screens/rest_demo.dart
@@ -1,10 +1,8 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:http/http.dart' as http;
-import 'package:state_change_demo/src/models/post.model.dart';
-import 'package:state_change_demo/src/models/user.model.dart';
+import 'package:state_change_demo/models/post.model.dart';
+import 'package:state_change_demo/utils/post_card.dart';
+import 'package:state_change_demo/utils/post_controller.dart';
 
 class RestDemoScreen extends StatefulWidget {
   const RestDemoScreen({super.key});
@@ -35,14 +33,14 @@ class _RestDemoScreenState extends State<RestDemoScreen> {
         actions: [
           IconButton(
               onPressed: () {
-                showNewPostFunction(context);
+                showAddEditPostDialog(context);
               },
               icon: const Icon(Icons.add))
         ],
       ),
       body: SafeArea(
-        child: ListenableBuilder(
-            listenable: controller,
+        child: AnimatedBuilder(
+            animation: controller,
             builder: (context, _) {
               if (controller.error != null) {
                 return Center(
@@ -51,23 +49,16 @@ class _RestDemoScreenState extends State<RestDemoScreen> {
               }
 
               if (!controller.working) {
-                return Center(
-                  child: SingleChildScrollView(
-                      padding: const EdgeInsets.all(16),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          for (Post post in controller.postList)
-                            Container(
-                                padding: const EdgeInsets.all(8),
-                                margin: const EdgeInsets.only(bottom: 8),
-                                decoration: BoxDecoration(
-                                    border:
-                                        Border.all(color: Colors.blueAccent),
-                                    borderRadius: BorderRadius.circular(16)),
-                                child: Text(post.toString()))
-                        ],
-                      )),
+                return ListView.builder(
+                  padding: const EdgeInsets.all(16),
+                  itemCount: controller.postList.length,
+                  itemBuilder: (context, index) {
+                    Post post = controller.postList[index];
+                    return PostCard(
+                      post: post,
+                      controller: controller,
+                    );
+                  },
                 );
               }
               return const Center(
@@ -81,200 +72,104 @@ class _RestDemoScreenState extends State<RestDemoScreen> {
     );
   }
 
-  showNewPostFunction(BuildContext context) {
-    AddPostDialog.show(context, controller: controller);
+  void showAddEditPostDialog(BuildContext context, {Post? post}) {
+    showDialog(
+      context: context,
+      builder: (dContext) => AddEditPostDialog(
+        controller: controller,
+        post: post,
+      ),
+    );
   }
 }
-
-class AddPostDialog extends StatefulWidget {
-  static show(BuildContext context, {required PostController controller}) =>
-      showDialog(
-          context: context, builder: (dContext) => AddPostDialog(controller));
-  const AddPostDialog(this.controller, {super.key});
-
+class AddEditPostDialog extends StatefulWidget {
   final PostController controller;
+  final Post? post;
+
+  const AddEditPostDialog({required this.controller, this.post, super.key});
 
   @override
-  State<AddPostDialog> createState() => _AddPostDialogState();
+  State<AddEditPostDialog> createState() => _AddEditPostDialogState();
 }
 
-class _AddPostDialogState extends State<AddPostDialog> {
+class _AddEditPostDialogState extends State<AddEditPostDialog> {
+  final _formKey = GlobalKey<FormState>();
   late TextEditingController bodyC, titleC;
+  bool isEdit = false;
 
   @override
   void initState() {
     super.initState();
-    bodyC = TextEditingController();
-    titleC = TextEditingController();
+    bodyC = TextEditingController(text: widget.post?.body ?? '');
+    titleC = TextEditingController(text: widget.post?.title ?? '');
+    isEdit = widget.post != null;
   }
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       contentPadding: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
-      title: const Text("Add new post"),
+      title: Text(isEdit ? "Edit Post" : "Add new post"),
       actions: [
         ElevatedButton(
           onPressed: () async {
-            widget.controller.makePost(
-                title: titleC.text.trim(), body: bodyC.text.trim(), userId: 1);
-            Navigator.of(context).pop();
+            if (_formKey.currentState!.validate()) {
+              if (isEdit) {
+                await widget.controller.updatePost(
+                  id: widget.post!.id,
+                  title: titleC.text.trim(),
+                  body: bodyC.text.trim(),
+                  userId: widget.post!.userId,
+                );
+              } else {
+                await widget.controller.makePost(
+                  title: titleC.text.trim(),
+                  body: bodyC.text.trim(),
+                  userId: 1,
+                );
+              }
+              Navigator.of(context).pop();
+            }
           },
-          child: const Text("Add"),
+          child: Text(isEdit ? "Update" : "Add"),
         )
       ],
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text("Title"),
-          Flexible(
-            child: TextFormField(
+      content: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text("Title"),
+            TextFormField(
               controller: titleC,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter a title';
+                }
+                return null;
+              },
             ),
-          ),
-          const Text("Content"),
-          Flexible(
-            child: TextFormField(
+            const SizedBox(height: 8),
+            const Text("Content"),
+            TextFormField(
               controller: bodyC,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter content';
+                }
+                return null;
+              },
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
-  }
-}
-
-class PostController with ChangeNotifier {
-  Map<String, dynamic> posts = {};
-  bool working = true;
-  Object? error;
-
-  List<Post> get postList => posts.values.whereType<Post>().toList();
-
-  clear() {
-    error = null;
-    posts = {};
-    notifyListeners();
-  }
-
-  Future<Post> makePost(
-      {required String title,
-      required String body,
-      required int userId}) async {
-    try {
-      working = true;
-      if(error != null ) error = null;
-      print(title);
-      print(body);
-      print(userId);
-      http.Response res = await HttpService.post(
-          url: "https://jsonplaceholder.typicode.com/posts",
-          body: {"title": title, "body": body, "userId": userId});
-      if (res.statusCode != 200 && res.statusCode != 201) {
-        throw Exception("${res.statusCode} | ${res.body}");
-      }
-
-      print(res.body);
-
-      Map<String, dynamic> result = jsonDecode(res.body);
-
-      Post output = Post.fromJson(result);
-      posts[output.id.toString()] = output;
-      working = false;
-      notifyListeners();
-      return output;
-    } catch (e, st) {
-      print(e);
-      print(st);
-      error = e;
-      working = false;
-      notifyListeners();
-      return Post.empty;
-    }
-  }
-
-  Future<void> getPosts() async {
-    try {
-      working = true;
-      clear();
-      List result = [];
-      http.Response res = await HttpService.get(
-          url: "https://jsonplaceholder.typicode.com/posts");
-      if (res.statusCode != 200 && res.statusCode != 201) {
-        throw Exception("${res.statusCode} | ${res.body}");
-      }
-      result = jsonDecode(res.body);
-
-      List<Post> tmpPost = result.map((e) => Post.fromJson(e)).toList();
-      posts = {for (Post p in tmpPost) "${p.id}": p};
-      working = false;
-      notifyListeners();
-    } catch (e, st) {
-      print(e);
-      print(st);
-      error = e;
-      working = false;
-      notifyListeners();
-    }
-  }
-}
-
-class UserController with ChangeNotifier {
-  Map<String, dynamic> users = {};
-  bool working = true;
-  Object? error;
-
-  List<User> get userList => users.values.whereType<User>().toList();
-
-  getUsers() async {
-    try {
-      working = true;
-      List result = [];
-      http.Response res = await HttpService.get(
-          url: "https://jsonplaceholder.typicode.com/users");
-      if (res.statusCode != 200 && res.statusCode != 201) {
-        throw Exception("${res.statusCode} | ${res.body}");
-      }
-      result = jsonDecode(res.body);
-
-      List<User> tmpUser = result.map((e) => User.fromJson(e)).toList();
-      users = {for (User u in tmpUser) "${u.id}": u};
-      working = false;
-      notifyListeners();
-    } catch (e, st) {
-      print(e);
-      print(st);
-      error = e;
-      working = false;
-      notifyListeners();
-    }
-  }
-
-  clear() {
-    users = {};
-    notifyListeners();
-  }
-}
-
-class HttpService {
-  static Future<http.Response> get(
-      {required String url, Map<String, dynamic>? headers}) async {
-    Uri uri = Uri.parse(url);
-    return http.get(uri, headers: {
-      'Content-Type': 'application/json',
-      if (headers != null) ...headers
-    });
-  }
-
-  static Future<http.Response> post(
-      {required String url,
-      required Map<dynamic, dynamic> body,
-      Map<String, dynamic>? headers}) async {
-    Uri uri = Uri.parse(url);
-    return http.post(uri, body: jsonEncode(body), headers: {
-      'Content-Type': 'application/json',
-      if (headers != null) ...headers
-    });
   }
 }
